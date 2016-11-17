c961e3a3060f46bf198c38ddff909e89658afcfb
peyman36 <peymansarijloo@yahoo.com>
2016-11-17 13:31:15 +0330

    anglurized the project

---
+++ app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('CounterApp', [])
+.controller('CounterController', CounterController);
+
+CounterController.$inject = ['$scope'];
+function CounterController($scope) {
+
+  $scope.showNumberOfWatchers = function () {
+
+  };
+
+}
+
+})();
+(function () {
+'use strict';
+
+angular.module('CounterApp', [])
+.controller('CounterController', CounterController);
+
+CounterController.$inject = ['$scope'];
+function CounterController($scope) {
+
+  $scope.showNumberOfWatchers = function () {
+  console.log($scope);	
+  };
+
+}
+
+})();
